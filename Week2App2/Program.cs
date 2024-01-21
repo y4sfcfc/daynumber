8ffed94a61f28be98c39072b9f2ac12d47a82510
Yusifli Yusif <yusifry@code.local>
2024-01-21 19:54:41 +0400

    weekdays yusif

---
+++ Week2App2/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Week2App2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome our app");
+
+            Console.WriteLine("Enter day number for result");
+            int daynumber = int.Parse(Console.ReadLine());
+            if (daynumber == 1)
+            {
+                Console.WriteLine("Watching file");
+            }
+            else if (daynumber == 2)
+            {
+                Console.WriteLine("Playing footbal");
+            }
+            else if (daynumber == 3)
+            {
+                Console.WriteLine("oing task");
+            }
+            else if (daynumber == 4)
+            {
+                Console.WriteLine("xxxxxx");
+            }
+            else if (daynumber == 5)
+            {
+                Console.WriteLine("yyyyy");
+            }
+            else if (daynumber ==6)
+            {
+                Console.WriteLine("zzzz");
+            }
+            else if(daynumber ==7)
+            {
+                Console.WriteLine("ddd");
+            }
+
+
+
+
+
+
+
+        }
+        }
+}
